a75092efa57084013c3f20794de835d065557507
Kendra McDaniels <kendramcdaniels01@gmail.com>
2023-10-05 03:07:11 +0000

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+  return (
+    <header>
+    <h1>Lab 01</h1>
+    </header>
+  )
+}
+}
+
+export default Header;
