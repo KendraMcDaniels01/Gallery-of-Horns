f4ea3d55b66c1e9c15028477092deb8feebb989b
Kendra McDaniels <kendramcdaniels01@gmail.com>
2023-10-07 17:07:06 +0000

    lab02

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap'
 import HornedBeast from './HornedBeast';
 import hornedBeastValues from '../assets/data.json'
 
@@ -7,9 +8,21 @@ import hornedBeastValues from '../assets/data.json'
 class Gallery extends React.Component {
   render() {
     return (
-      <section>
-        {hornedBeastValues.map((beast, index) => <HornedBeast key={index} title={beast.title} image_url={beast.image_url} horns={beast.horns} description={beast.description} />)}
-      </section>
+      <Container>
+        <Row xs={1} sm={2} md={3} lg={4} className="g-4">
+        {hornedBeastValues.map((beast, index) => 
+        <Col key={index}>
+        <HornedBeast 
+        key={index} 
+        title={beast.title} 
+        image_url={beast.image_url} 
+        horns={beast.horns} 
+        description={beast.description} 
+        />
+        </Col>
+        )}
+        </Row>
+      </Container>
     );
   }
 }
--- src/components/HornedBeast.jsx
+++ src/components/HornedBeast.jsx
@@ -3,26 +3,29 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 class HornedBeast extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      isFavorite: false,
+      favorites: 0,
     };
   }
 
-  handleClick= () => {
-    this.setState({isFavorite: !this.state.isFavorite})
-  }
+  handleClick = () => {
+    this.setState((currentFavorites) => ({
+      favorites: currentFavorites.favorites +1,
+    }))
+  };
+
   render () {
     return (
       <Card>
         <Card.Img variant="top" src={this.props.image_url} />
         <Card.Body>
           <Card.Title>{this.props.name}</Card.Title>
-          <Card.Text>{'Favorite card: '+this.state.isFavorite}</Card.Text>
+          <Card.Text>{'Favorite card: '+this.state.favorites}</Card.Text>
           <Card.Text>{this.props.description}</Card.Text>
           <Card.Text>{'number of horns '+this.props.horns}</Card.Text>
-          <Button variant="primary" onClick ={this.handleClick}>Favorite</Button>
+          <Button variant="primary" onClick ={this.handleClick}>💕 Favorite</Button>
         </Card.Body>
       </Card>
     );
